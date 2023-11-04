5cbc9e12b68f20e3b6baf7f7e1eab7d10075063a
Jeanadoan <140124143+Jeanadoan@users.noreply.github.com>
2023-11-03 19:11:10 -0700

    Add files via upload

---
+++ loop/src/loop/loop.java
@@ -0,0 +1,12 @@
+package loop;
+
+public class loop {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		for (int i=2; i<=100 ; i+=2) {
+		System.out.println (i);
+	}
+	}
+}
---
+++ matriex/src/matriex/sasda.java
@@ -0,0 +1,19 @@
+package matriex;
+
+public class sasda {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int [] [] matrix = {
+		{10, 20 , 30},
+		{40, 50, 60},
+		{70, 80,90}
+	};{
+	for (int i=0;i< matrix.length; i++) {
+		for(int j =0; j < matrix[i].length;j++) {
+			System.out.println (matrix[i][j] + "");
+		}}
+	}
+	}
+
+}
